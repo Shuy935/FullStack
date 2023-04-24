7b6ae31bd24da7b1dad1b96f0261cfcf61abd766
Shuy935 <jessus_2500@hotmail.com>
2023-04-24 09:59:16 -0600

    codigos http

--- servidor.js
+++ servidor.js
@@ -21,3 +21,21 @@ var mimeTypes = {
     '.jpg': 'image/jpg',
     '.gif': 'image/gif'
 }
+fs.readFile(filePath, function(error, content){
+    if(error){
+        if(error.code=='ENOENT'){
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200,{ 'Content-Type': contentType});
+                response.end(content, 'utf-8');
+            });
+        }
+        else{
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: '+error.code);
+            response.end();
+        }
+    }else{
+        response.writeHead(200,{'Content-Type': contentType});
+        response.end(content, 'utf-8');
+    }
+});
